3b9a294d1fd39ca04f8e1f728ea289115ddba8fb
byMagg <byMag@hotmail.es>
2023-04-20 13:09:24 +0200

    update

--- api/app/controllers/auth.js
+++ api/app/controllers/auth.js
@@ -6,15 +6,19 @@ const sendJSONresponse = (res, status, content) => {
     res.json(content);
 };
 
-
 /* GET api/generate_token */
 const generateToken = async (req, res) => {
     try {
         const response = await axios.post(`https://accounts.spotify.com/api/token?grant_type=client_credentials&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`,)
         config.TOKEN_SECRET = response.data.access_token
-        sendJSONresponse(res, 200, config.TOKEN_SECRET)
+        sendJSONresponse(res, 200, response.data)
     } catch (error) {
-        console.log(error)
+        sendJSONresponse(res, 400, {
+            "error": {
+                "code": "400",
+                "message": "La solicitud es incorrecta. Verifique que la información proporcionada sea válida y esté completa."
+            }
+        })
     }
 };
 
