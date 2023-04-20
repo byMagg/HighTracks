6dedb8b46120f7119cde42be8c438d4c9a1d744a
byMagg <byMag@hotmail.es>
2023-04-20 13:03:38 +0200

    add error 404

--- api/app.js
+++ api/app.js
@@ -1,7 +1,6 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const logger = require('morgan');
+
 require('./app/models/db');
 require('dotenv').config();
 
@@ -9,16 +8,19 @@ const apiRouter = require('./app/routes/index');
 
 const app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use((req, res) => {
+  res.status(404).send({
+    "error": {
+      "code": "404",
+      "message": "El recurso solicitado no se encontró en el servidor."
+    }
+  })
 });
 
 module.exports = app;
