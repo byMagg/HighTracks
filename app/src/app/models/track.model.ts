0563dd9bce546ee955fe78bc7a1e1d8d105e627a
byMagg <byMag@hotmail.es>
2023-05-08 12:24:48 +0200

    separate models

--- app/src/app/models/track.model.ts
+++ app/src/app/models/track.model.ts
@@ -1,4 +1,6 @@
+import { Album } from "./album.model"
 import { Coords } from "./coords.model"
+import { Comment } from "./comment.model"
 
 export class Track {
   _id: string
@@ -13,45 +15,4 @@ export class Track {
     this.name = name
     this.album = album
   }
-}
-
-export class Album {
-  artists: Artist[]
-  id?: string
-  images: Image[]
-  name: string
-  release_date?: string
-  release_date_precision?: string
-  total_tracks?: number
-
-  constructor(artist: string, name: string, url: string) {
-    this.name = name
-    this.artists = [
-      {
-        name: artist
-      }
-    ]
-    this.images = [
-      {
-        url: url
-      }
-    ]
-  }
-}
-
-export interface Artist {
-  id?: string
-  name: string
-}
-
-export interface Image {
-  height?: number
-  url: string
-  width?: number
-}
-
-export interface Comment {
-  author: string
-  text: string
-  score: number
 }
