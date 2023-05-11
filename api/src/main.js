f0e3fc3d9d4492a82bec532ab001cf172ded46ed
byMagg <byMag@hotmail.es>
2023-05-11 23:09:27 +0200

    add cert

--- api/src/main.js
+++ api/src/main.js
@@ -1,5 +1,21 @@
 const app = require("./app");
 const port = 3000
-app.listen(port, () => {
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+};
+try {
+  options = {
+    cert: fs.readFileSync('/etc/letsencrypt/live/webapps.masterdevevelopment.tech/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/webapps.masterdevevelopment.tech/privkey.pem')
+  };
+} catch (error) {
+}
+
+https.createServer(options, app).listen(3000, () => {
   console.log(`HighTracks API running on port ${port}`)
 })
+// app.listen(port, () => {
+//   console.log(`HighTracks API running on port ${port}`)
+// })
