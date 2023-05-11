25a559509ff2e1fc09b1ff782dcaf87194e95494
byMagg <byMag@hotmail.es>
2023-05-11 23:18:48 +0200

    revert

--- api/src/main.js
+++ api/src/main.js
@@ -1,21 +1,5 @@
 const app = require("./app");
 const port = 3000
-const https = require('https');
-const fs = require('fs');
-
-const options = {
-};
-try {
-  options = {
-    cert: fs.readFileSync('/etc/letsencrypt/live/webapps.masterdevevelopment.tech/fullchain.pem'),
-    key: fs.readFileSync('/etc/letsencrypt/live/webapps.masterdevevelopment.tech/privkey.pem')
-  };
-} catch (error) {
-}
-
-https.createServer(options, app).listen(3000, () => {
+app.listen(port, () => {
   console.log(`HighTracks API running on port ${port}`)
 })
-// app.listen(port, () => {
-//   console.log(`HighTracks API running on port ${port}`)
-// })
