97f54878c12eb1b5724d15c04900d4337aa7ba29
byMagg <byMag@hotmail.es>
2023-05-19 20:03:24 +0200

    working android app

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'app',
+  appId: 'io.ionic.hightracks',
+  appName: 'HighTracks',
   webDir: 'www',
   bundledWebRuntime: false
 };
